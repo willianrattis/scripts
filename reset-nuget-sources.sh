dea6f87e01f46004eccc3ea007a777355081e459
Willian Santa Rosa Rattis <willian.rattis@hotmail.com>
2024-02-07 15:38:19 -0300

    add update repositories with git pull

--- reset-nuget-sources.sh
+++ reset-nuget-sources.sh
@@ -7,8 +7,6 @@ RESET=$(tput sgr0)
 
 # Step 1: Ask for user and password
 echo "${BOLD}${YELLOW}Please enter your credentials to re-add the NuGet sources:${RESET}"
-echo -n "Enter your username: "
-read username
 echo -n "Enter your password: "
 read -s password
 echo
@@ -23,8 +21,8 @@ echo
 # Step 2: Remove each NuGet source individually
 echo "${BOLD}${YELLOW}Removing NuGet sources...${RESET}"
 dotnet nuget remove source "nuget.org"
-dotnet nuget remove source "Online"
 dotnet nuget remove source "Core"
+dotnet nuget remove source "Via"
 echo
 echo
 
@@ -37,8 +35,8 @@ echo
 
 # Step 4: Re-add sources
 echo "${BOLD}${YELLOW}Re-adding NuGet sources...${RESET}"
-dotnet nuget add source https://<your-personal-repositories>/repository/nuget-online/ --name Online --username $username --password $password --store-password-in-clear-text
-dotnet nuget add source https://<your-personal-repositories>/repository/repository-core/ --name Core --username $username --password $password --store-password-in-clear-text
+dotnet nuget add source https://nexus.viavarejo.com.br/repository/nuget-online/ --name Core --username 4960017680 --password $password --store-password-in-clear-text
+dotnet nuget add source https://nexus.viavarejo.com.br/repository/viaunica-core/ --name Via --username 4960017680 --password $password --store-password-in-clear-text
 dotnet nuget add source https://api.nuget.org/v3/index.json --name nuget.org
 echo
 echo
